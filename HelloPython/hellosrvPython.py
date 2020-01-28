90a11944c2e12b5cb09ce4183d7f0f8b6987e542
Ubuntu <ubuntu@ip-172-31-91-199.ec2.internal>
2020-01-28 04:17:18 +0000

    server update

--- HelloPython/hellosrvPython.py
+++ HelloPython/hellosrvPython.py
@@ -10,7 +10,7 @@ print("Starting Python hellosrv")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
+    s.bind(('', PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
